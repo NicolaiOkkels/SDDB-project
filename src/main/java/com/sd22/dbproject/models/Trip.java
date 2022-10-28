9ba95617408c8ff7632a502d7d8cea3e197d8fd7
erhan basaran <e.basaran.111@gmail.com>
2022-10-28 13:23:44 +0200

    different model classes created

---
+++ src/main/java/com/sd22/dbproject/models/Trip.java
@@ -0,0 +1,13 @@
+package com.sd22.dbproject.models;
+
+public class Trip {
+/*      `trip_id` INT NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(500) NOT NULL,
+  `price` DECIMAL(9,2) NOT NULL,
+  `length` VARCHAR(255) NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `availabilty` VARCHAR(255) NOT NULL,
+  `rating_total` DECIMAL(2,1) NULL,
+  `locations_location_id` INT NOT NULL,
+            `packages_package_id` INT NOT NULL,*/
+}
