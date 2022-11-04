045f27a9fe0a257e0dc86ea8fdbc6b136c66c429
nicolaiokkels <nico902t@stud.kea.dk>
2022-11-04 12:23:06 +0100

    added user service, repository & endpoints for reference

--- src/main/java/com/sd22/dbproject/DbProjectApplication.java
+++ src/main/java/com/sd22/dbproject/DbProjectApplication.java
@@ -2,8 +2,10 @@ package com.sd22.dbproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableSwagger2 TODO: fix bug where enabling this make the app crash
 public class DbProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sd22/dbproject/repositories/UserRepository.java
+++ src/main/java/com/sd22/dbproject/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.sd22.dbproject.repositories;
 
-public class UserRepository {
+import com.sd22.dbproject.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
