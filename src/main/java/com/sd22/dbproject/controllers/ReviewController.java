711de9c7485e20b0252db848d9198133ecaf5535
nicolaiokkels <nico902t@stud.kea.dk>
2022-11-10 19:33:11 +0100

    fixed swagger ui + changed all endpoints to reflect their function

--- src/main/java/com/sd22/dbproject/controllers/ReviewController.java
+++ src/main/java/com/sd22/dbproject/controllers/ReviewController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/review")
+@RequestMapping("/api/reviews")
 public class ReviewController {
 }
