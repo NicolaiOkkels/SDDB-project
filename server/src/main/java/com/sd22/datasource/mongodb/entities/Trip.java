7a0aed2b9e384756cacd3c7f5ee7172d232a1845
kasper <kasperkbf@hotmail.com>
2022-12-15 20:12:29 +0100

    initial mongodb

---
+++ server/src/main/java/com/sd22/datasource/mongodb/entities/Trip.java
@@ -0,0 +1,74 @@
+package com.sd22.datasource.mongodb.entities;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "trips")
+
+public class Trip {
+    @Id
+    private int tripId;
+    private String description;
+    private double price;
+    private String length;
+    private String title;
+    private String availabilty;
+    private Double ratingTotal;
+
+    public int getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(int tripId) {
+        this.tripId = tripId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAvailabilty() {
+        return availabilty;
+    }
+
+    public void setAvailabilty(String availabilty) {
+        this.availabilty = availabilty;
+    }
+
+    public Double getRatingTotal() {
+        return ratingTotal;
+    }
+
+    public void setRatingTotal(Double ratingTotal) {
+        this.ratingTotal = ratingTotal;
+    }
+}
